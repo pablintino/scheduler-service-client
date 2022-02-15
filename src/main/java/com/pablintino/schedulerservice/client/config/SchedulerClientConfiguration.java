cbd2dd83f3c7f80ae4bcb80bf3c2b3ebc628dc42
Pablo Rodriguez Nava <pablintino@hotmail.com>
2022-02-15 13:03:06 +0100

    Changed client to accept JSON AMQP messages and added cron test IT test

--- src/main/java/com/pablintino/schedulerservice/client/config/SchedulerClientConfiguration.java
+++ src/main/java/com/pablintino/schedulerservice/client/config/SchedulerClientConfiguration.java
@@ -1,8 +1,14 @@
 package com.pablintino.schedulerservice.client.config;
 
 import com.pablintino.schedulerservice.client.SchedulerServiceClient;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +24,33 @@ public class SchedulerClientConfiguration {
     }
 
     @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+    public ConnectionFactory connectionFactory(@Value("${spring.rabbitmq.host:localhost}") String host, @Value("${spring.rabbitmq.port:5672}") int port) {
+        return new CachingConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
         return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2Converter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory = new SimpleRabbitListenerContainerFactory();
+        simpleRabbitListenerContainerFactory.setConnectionFactory(connectionFactory);
+        simpleRabbitListenerContainerFactory.setMessageConverter(jackson2Converter());
+        simpleRabbitListenerContainerFactory.setMaxConcurrentConsumers(20);
+        return simpleRabbitListenerContainerFactory;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2Converter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
